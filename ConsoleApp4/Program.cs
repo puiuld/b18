5d417270802e31965703ac9ec5fc4d471fa49cfa
puiuld <puiuld47@gmail.com>
2025-05-27 11:10:33 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            
+            if (x > 0)
+            {
+                ++x;
+            }
+            if (x < 0)
+            {
+                x -= 2;
+            }
+            if(x == 0)
+            {
+                x = 10;
+            }
+            Console.Write($"x = {x}");
+            Console.ReadLine();
+
+        }
+    }
+}
