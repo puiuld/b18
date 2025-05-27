3adfa955c050811c7e000481db7fc0029272193e
puiuld <puiuld47@gmail.com>
2025-05-27 11:02:19 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            int y = (x > 0) ? ++x : x -= 2;
+            Console.Write($"y = {y}");
+            Console.ReadLine();
+        }
+    }
+}
